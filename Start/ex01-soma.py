4cc6b16530eeebf6e3e1194901c24dc617cdc779
Brunna Brito <brunna.raphaela1@gmail.com>
2025-08-13 21:38:36 -0300

    comit

---
+++ Start/ex01-soma.py
@@ -0,0 +1,10 @@
+#Soma ex001
+#Entrada de dados
+a = int(input("Digite um valor:"))
+b = int(input("Digite outro valor:"))
+
+#Processamento
+soma = a +b
+
+#Saída de dados
+print("SOMA = ", soma)
---
+++ Start/ex02-produto.py
@@ -0,0 +1,10 @@
+#Produto simples ex002
+#entrada de dados
+a = int(input("Digite um valor:"))
+b = int(input("Digite outro valor:"))
+
+#Processamento
+PROD = a*b
+
+#Saída de dados
+print("PROD = {}".format(PROD))
