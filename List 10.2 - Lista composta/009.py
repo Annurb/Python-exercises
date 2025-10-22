ce6eac88423131ab6c81eb43d3806bb2131efe9d
Brunna Brito <brunna.raphaela1@gmail.com>
2025-10-22 19:59:58 -0300

    Update 009.py

--- List 10.2 - Lista composta/009.py
+++ List 10.2 - Lista composta/009.py
@@ -1,26 +1,36 @@
 n, m = map(int, input().split())
-matriz = []
-jogador = []
-soma = []
 pontuacao = 0
+total = []
+matriz = []
+somatoria = []
 for c in range(n):
-    jogador = []
     jogador = list(map(int, input().split()))
     matriz.append(jogador[:])
-for l in range(n):
-    for d in range(m):
-        if l == 0:
-            jogador.append(matriz[l][d])
-            soma.append(matriz[l][d])
-        else:
-            jogador[d] = jogador[d]*matriz[l][d]
-            jogador.pop(d+1)
-            soma[d] = jogador[d]+matriz[l][d]
-            jogador.pop(d+1)
-print(jogador)
-for num in range(len(jogador)):
-    if jogador[num]>0:
+    jogador = []
+
+for i in range(n):
+    gols = matriz[i]
+    marcou = True
+    for g in gols:
+        if g == 0:
+            marcou = False
+            break
+    if marcou:
         pontuacao +=1
-print(soma)
-print(pontuacao)
-print(matriz)
+
+    total = 0
+    for g in gols:
+        total+=g
+    somatoria.append(total)
+
+max = 0
+for t in somatoria:
+    if t> max:
+        max = t
+
+artilheiros = []
+for j in range(n):
+    if somatoria[j] == max:
+        artilheiros.append(j+1)
+print(f'Número de jogadores que pontuaram em todas as partidas: {pontuacao}' )   
+print(f'Artilehiro(s): jogador {artilheiros}({max} gols)')
